402032aa4721bf410ef699bc6415893bfe2b2b2a
Micah Jackson <thehazelwize@gmail.com>
2022-04-01 23:00:03 -0400

    First commit of daily coding challenges

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/5f70c883e10f9e0001c89673
+
+const flip=(d, a)=>{
+    return d == 'R'? a.sort((a , b)=>a - b) : a.sort((a , b) => b - a)
+  }
+
+//https://www.codewars.com/kata/5861d28f124b35723e00005e
+  const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    return distanceToPump <= mpg * fuelLeft
+  };
