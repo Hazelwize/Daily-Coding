f49b7cc9e76dc0055a0aef1c50abb5ba962c866b
Micah Jackson <thehazelwize@gmail.com>
2022-04-04 23:22:39 -0400

    daily code challenge

--- js/main.js
+++ js/main.js
@@ -22,4 +22,15 @@ const flip=(d, a)=>{
     return !((f1 % 2 == 0 && f2 % 2 == 0)||(f1 % 2 != 0 && f2 % 2 != 0))
   }
 
-  
+  //https://www.codewars.com/kata/5672a98bdbdd995fad00000f
+  const rps = (p1, p2) => {
+    if(p1 == 'rock' && p2 == "scissors" ||
+      p1 == 'paper' && p2 == 'rock' ||
+      p1 == 'scissors' && p2 == 'paper'){
+      return "Player 1 won!"
+    }else if(p1 == p2){
+      return 'Draw!'
+    }else{
+      return "Player 2 won!"
+    }
+  };
