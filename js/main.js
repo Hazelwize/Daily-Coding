e631b3713dd86e77262adca51f5186315d57ff6c
Micah Jackson <thehazelwize@gmail.com>
2022-04-06 16:21:04 -0400

    created function for filtering array elements

--- js/main.js
+++ js/main.js
@@ -55,3 +55,10 @@ function stringy(size) {
   } 
   return value.join('');
 }
+
+
+//https://www.codewars.com/kata/5769b3802ae6f8e4890009d2
+
+function removeEveryOther(arr){
+  return arr.filter((e, i) =>  i % 2 == 0)
+}
