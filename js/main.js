571d4d79941895b407794f6ddd503dddea7e06cd
Micah Jackson <thehazelwize@gmail.com>
2022-04-29 22:41:10 -0400

    addedfunction to sort unique characters in order

--- js/main.js
+++ js/main.js
@@ -308,3 +308,14 @@ function getMiddle(s){
     }
   }).join('')
 }
+
+//Unique in Order
+//https://www.codewars.com/kata/54e6533c92449cc251001667
+
+var uniqueInOrder=function(iterable){
+  return [...iterable].filter((e, i) => {
+    if(e != iterable[(i+1)]){
+      return e;
+    }
+  })
+}
