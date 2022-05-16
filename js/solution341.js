b28125709989735d033d175503ae5c8593eaacca
Micah Jackson <thehazelwize@gmail.com>
2022-05-15 21:07:19 -0400

    added solution 341

---
+++ js/solution341.js
@@ -0,0 +1,21 @@
+// Terminal game move function
+
+// In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+// Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+// Example:
+
+// move(3, 6) should equal 15
+
+//P: pos -position, roll - dice roll
+//R: new position
+//E:
+//P: return dice times two plus position number
+
+function rollDice(pos, roll){
+    return roll * 2 + pos
+}
+
+
+rollDice(4, 6) // 16
+rollDice(10,4)// 18
