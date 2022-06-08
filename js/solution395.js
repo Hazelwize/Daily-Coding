edcc741818f7275fe0389bab4d4c2311b7cb11cb
Micah Jackson <thehazelwize@gmail.com>
2022-06-08 17:36:59 -0400

    added solution 395

---
+++ js/solution395.js
@@ -0,0 +1,9 @@
+// Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
+function getSize(w, h, d){
+  
+    return [(h * w * 2)+(h * d * 2)+(d * w * 2), (w * h * d)]
+    
+  }
+  
+  
