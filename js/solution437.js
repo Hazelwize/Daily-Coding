ba73442122f08d8df1b504e1a83220aa4cb3d2f2
Micah Jackson <thehazelwize@gmail.com>
2022-06-24 16:30:26 -0400

    added solution 436 and 437

---
+++ js/solution437.js
@@ -0,0 +1,12 @@
+// Your task is to write function factorial.
+
+function factorial(n){
+    let result = n
+    if(n >= 1){
+      for(n ; n > 1; n--){
+        result *= n-1
+      }
+      return result 
+    }
+    return 1
+  }
