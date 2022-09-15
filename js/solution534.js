debff054f417e0cc29e76dacf79845f288b9d60f
Micah Jackson <thehazelwize@gmail.com>
2022-09-15 14:26:28 -0400

    added solution 534

---
+++ js/solution534.js
@@ -0,0 +1,23 @@
+// Your task, is to create NxN multiplication table, of size provided in parameter.
+
+// for example, when given size is 3:
+
+// 1 2 3
+// 2 4 6
+// 3 6 9
+
+// for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
+
+
+multiplicationTable = function(size) {
+    let table = []
+    for(let i = 1; i <= size; i++){
+      let current = []
+      for(let j = 1; j <= size; j++){
+        current.push(i * j)
+      }
+      table.push(current)
+    }
+    return table
+}
+  
