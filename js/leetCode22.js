21cdd9195f63b9fab430a2270afa8d6233e6fc13
Micah Jackson <thehazelwize@gmail.com>
2022-12-19 19:28:24 -0500

    add leetCode 22

---
+++ js/leetCode22.js
@@ -0,0 +1,62 @@
+// 804. Unique Morse Code Words
+// Easy
+// 2.1K
+// 1.4K
+// Companies
+
+// International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+
+//     'a' maps to ".-",
+//     'b' maps to "-...",
+//     'c' maps to "-.-.", and so on.
+
+// For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+// [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+
+// Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+//     For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+
+// Return the number of different transformations among all words we have.
+
+//  
+
+// Example 1:
+
+// Input: words = ["gin","zen","gig","msg"]
+// Output: 2
+// Explanation: The transformation of each word is:
+// "gin" -> "--...-."
+// "zen" -> "--...-."
+// "gig" -> "--...--."
+// "msg" -> "--...--."
+// There are 2 different transformations: "--...-." and "--...--.".
+
+// Example 2:
+
+// Input: words = ["a"]
+// Output: 1
+
+//  
+
+// Constraints:
+
+//     1 <= words.length <= 100
+//     1 <= words[i].length <= 12
+//     words[i] consists of lowercase English letters.
+
+
+var uniqueMorseRepresentations = function(words) {
+    const codes = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+    let result = ''
+    let str = words.join(' ')
+    for(let i = 0 ; i < str.length; i++){
+        if(str[i] === ' '){
+            result += ' '
+        }else{
+            result  += codes[str.charCodeAt(i) - 97]
+        }  
+    }
+    return new Set(result.split(' ')).size
+};
