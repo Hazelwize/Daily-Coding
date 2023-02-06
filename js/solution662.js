d13a7481ab8224d5e9e49a1deae34a7a5c63ae88
Micah Jackson <thehazelwize@gmail.com>
2023-02-06 17:23:56 -0500

    add solution 662

---
+++ js/solution662.js
@@ -0,0 +1,50 @@
+// Greed is a dice game played with five six-sided dice. Your mission, should you choose to accept it, is to score a throw according to these rules. You will always be given an array with five six-sided dice values.
+
+//  Three 1's => 1000 points
+//  Three 6's =>  600 points
+//  Three 5's =>  500 points
+//  Three 4's =>  400 points
+//  Three 3's =>  300 points
+//  Three 2's =>  200 points
+//  One   1   =>  100 points
+//  One   5   =>   50 point
+
+// A single die can only be counted once in each roll. For example, a given "5" can only count as part of a triplet (contributing to the 500 points) or as a single 50 points, but not both in the same roll.
+
+// Example scoring
+
+//  Throw       Score
+//  ---------   ------------------
+//  5 1 3 4 1   250:  50 (for the 5) + 2 * 100 (for the 1s)
+//  1 1 1 3 1   1100: 1000 (for three 1s) + 100 (for the other 1)
+//  2 4 4 5 4   450:  400 (for three 4s) + 50 (for the 5)
+
+// In some languages, it is possible to mutate the input to the function. This is something that you should never do. If you mutate the input, you will not be able to pass all the tests.
+
+function score( dice ) {
+    const nums = {}
+    for(const n of dice){
+      nums[n] = nums[n] + 1 || 1
+    }
+    let total = 0
+    for(const n in nums){
+      if(n === '1'){
+        if(nums[n] >= 3){
+          total += 1000 + ((nums[n] - 3) * 100)
+        }else{
+          total += nums[n] * 100
+        }
+      }
+      if(n === '5'){
+        if(nums[n] >= 3){
+          total += 500 + ((nums[n] - 3) * 50)
+        }else{
+          total += nums[n] * 50
+        }
+      }
+      if(n !== '1' && n !== '5' && nums[n] > 2){
+        total += n * 100
+      }
+    }
+    return total
+}
