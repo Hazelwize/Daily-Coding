29454a958d118f9f01fc6283e312bf63011fcd31
Micah Jackson <thehazelwize@gmail.com>
2023-04-26 16:18:37 -0400

    add solution 670

---
+++ js/solution670.js
@@ -0,0 +1,49 @@
+// The word i18n is a common abbreviation of internationalization in the developer community, used instead of typing the whole word and trying to spell it correctly. Similarly, a11y is an abbreviation of accessibility.
+
+// Write a function that takes a string and turns any and all "words" (see below) within that string of length 4 or greater into an abbreviation, following these rules:
+
+//     A "word" is a sequence of alphabetical characters. By this definition, any other character like a space or hyphen (eg. "elephant-ride") will split up a series of letters into two words (eg. "elephant" and "ride").
+//     The abbreviated version of the word should have the first letter, then the number of removed characters, then the last letter (eg. "elephant ride" => "e6t r2e").
+
+// Example
+
+// abbreviate("elephant-rides are really fun!")
+// //          ^^^^^^^^*^^^^^*^^^*^^^^^^*^^^*
+// // words (^):   "elephant" "rides" "are" "really" "fun"
+// //                123456     123     1     1234     1
+// // ignore short words:               X              X
+
+// // abbreviate:    "e6t"     "r3s"  "are"  "r4y"   "fun"
+// // all non-word characters (*) remain in place
+// //                     "-"      " "    " "     " "     "!"
+// === "e6t-r3s are r4y fun!"
+
+
+
+function abbreviate(string) {
+    let current = '';
+    let result = ''
+    for( let i = 0 ; i < string.length; i++){
+      if(string[i] < 'A' || string[i] > 'z'){
+        result = result + convertText(current) + string[i]
+        current = ''
+      }else{
+        current += string[i]
+      }
+    }
+    result += convertText(current)
+    return result
+}
+  
+function convertText(str){
+    if(!str){
+      return ''
+    }
+    if(str.length >= 4){
+      console.log(str)
+      return str[0] + String(str.length - 2) + str[str.length - 1] 
+    }else{
+      console.log(str)
+      return str
+    }
+}
